0462662771251e7f523de5c31f5de96db971b1f1
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-06-28 15:03:48 +0700

    Update method call dr reflectionclass

--- helper/ValidationUtil.php
+++ helper/ValidationUtil.php
@@ -8,9 +8,9 @@ class ValidationUtil
     $properties =  $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
     foreach ($properties as $property) {
       if (!$property->isInitialized($request)) {
-        throw new ValidationException("$property->name ga di set");
+        throw new ValidationException("{$property->getName()} ga di set");
       } else if (is_null($property->getValue($request))) {
-        throw new ValidationException("$property->name kosong");
+        throw new ValidationException("{$property->getName()} kosong");
       }
     }
     echo "Kamu Berhasil Login";
